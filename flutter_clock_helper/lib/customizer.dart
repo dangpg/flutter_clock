88265f21afd55c4feb429fdbd5779425ff00b3e1
Phi Dang <dangpg@users.noreply.github.com>
2019-12-23 18:07:30 +0100

    Remove border of customizer

--- flutter_clock_helper/lib/customizer.dart
+++ flutter_clock_helper/lib/customizer.dart
@@ -177,12 +177,6 @@ class _ClockCustomizerState extends State<ClockCustomizer> {
       child: AspectRatio(
         aspectRatio: 5 / 3,
         child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 2,
-              color: Theme.of(context).unselectedWidgetColor,
-            ),
-          ),
           child: widget._clock(_model),
         ),
       ),
